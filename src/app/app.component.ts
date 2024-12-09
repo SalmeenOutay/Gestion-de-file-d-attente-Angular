6305532afdbe692ab77d3ce909585ee378a570d6
otay salmeen <salmenotay96@gmail.com>
2024-12-09 21:55:42 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+
+
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent,HeaderComponent],
+  imports: [CommonModule,RouterModule,RouterOutlet,
+    HeaderComponent,FooterComponent,],
+    
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,43 +1,61 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router'
+import { SharedService } from '../shared.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit {
-  check: number = 0;
+export class LoginComponent {
+  
+  email: string = '';
+  password: string = '';
 
-  ngOnInit(): void {
-    this.toggleLoginBoxAutomatically();
-  }
+  constructor(
+    private sharedService: SharedService,
+    private router: Router
+  ) {}
 
-  toggleLoginBoxAutomatically(): void {
-    const cta = document.querySelector(".cta") as HTMLElement;
+  
+  onSubmit() {
+    const user = this.sharedService.users.find(
+      (u) => u.email === this.email && u.password === this.password
+    );
 
-    if (cta) {
-      const text = cta.nextElementSibling as HTMLElement;
-      const loginText = cta.parentElement as HTMLElement;
+    if (user) {
+      
+      this.sharedService.connected = true;
+      this.sharedService.current_user_id = user.id;
+      this.router.navigate(['/home']);
+      
+      Swal.fire({
+        title: 'loged in successfully',
+        text: 'You have been logged in successfully',
+        icon:'success',
+        confirmButtonText: 'ok',
+        customClass: {
+          popup: 'my-swal-popup',
+        },
+      });
 
-      if (text && loginText) {
-        // Automatically toggle the classes
-        text.classList.toggle('show-hide');
-        loginText.classList.toggle('expand');
-
-        if (this.check === 0) {
-          cta.innerHTML = "<i class='fas fa-chevron-up'></i>";
-          this.check++;
-        } else {
-          cta.innerHTML = "<i class='fas fa-chevron-down'></i>";
-          this.check = 0;
-        }
-      }
+    } else {
+      
+      Swal.fire({
+        title: 'password error or email not found',
+        text: 'Invalid email or password',
+        icon:'error',
+        confirmButtonText: 'ok',
+        customClass: {
+          popup: 'my-swal-popup',
+        },
+      });
     }
-  }
-  onclick(){
-    alert("login succsesfuly");
   }
 }
